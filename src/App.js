00d593a9f4fb6c0698602d9f07f2194235028120
Blackbullet99 <isaacokanigbe@gmail.com>
2023-02-08 19:59:45 -0800

    first axios api test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { firstAxiosInstance, secondAxiosInstance } from "./axios-test";
+import { useSearchParams } from "react-router-dom";
 
-function App() {
+const TestApp = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const session_id = searchParams.get("session_id");
+
+  const getDataHandler = async () => {
+    setLoading(true);
+    try {
+      const response = await firstAxiosInstance.get("");
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    }
+
+    setLoading(false);
+  };
+
+  const postDataHandler = async () => {
+    setLoading(true);
+    try {
+      const response = await firstAxiosInstance.post("", {
+        session_id: "un6beoydda13mcno4kv8lsady27motnz",
+      });
+      setData(response.data);
+
+      const response2 = await secondAxiosInstance.post("", {
+        session_id: "un6beoydda13mcno4kv8lsady27motnz",
+      });
+      setData(response2.data);
+    } catch (error) {
+      setError(error);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getDataHandler();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!data) return <p>No Data</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test App</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <button onClick={postDataHandler}>Post Data</button>
     </div>
   );
-}
+};
 
-export default App;
+export default TestApp;
