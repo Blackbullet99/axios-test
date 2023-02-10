0288bd483e701a02e116bb7f1968e6ea5efbd0a0
Blackbullet99 <isaacokanigbe@gmail.com>
2023-02-09 17:59:54 -0800

    updated test

--- src/App.js
+++ src/App.js
@@ -1,41 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { firstAxiosInstance, secondAxiosInstance } from "./axios-test";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const App = () => {
+  const location = useLocation();
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.location.href = "http://localhost:3000/axios-test";
+    }
+  }, [isLoggedIn]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://100014.pythonanywhere.com/api/login/",
+        {
+          username,
+          password,
+        }
+      );
+      if (response.data.status === "success") {
+        setIsLoggedIn(true);
+      } else {
+        throw new Error("Login failed");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const handleSubmit = async () => {
-      // event.preventDefault();
-
       setLoading(true);
 
       try {
-        const getData = await firstAxiosInstance.get("");
-        console.log(getData);
-      } catch (error) {
-        console.error(error);
-      }
+        const searchParams = new URLSearchParams(location.search);
+        const sessionId = searchParams.get("session_id");
+        const id = searchParams.get("id");
 
-      try {
-        const response = await firstAxiosInstance.post("", {
-          message: "its working",
-        });
-
-        console.log(response.data);
-
-        const url = new URL(response.request.responseURL);
-        const sessionId = url.searchParams.get("session_id");
-
-        console.log(sessionId);
         if (sessionId) {
+          const response = await firstAxiosInstance.get(
+            `?session_id=${sessionId}`
+          );
           setData({
             sessionId,
             responseData: response.data,
           });
         } else {
           throw new Error("Session ID not found in URL");
+        }
+
+        if (sessionId && id) {
+          const response = await secondAxiosInstance.get(
+            `?session_id=${sessionId}&id=${id}`
+          );
+          setData({
+            sessionId,
+            id,
+            responseData: response.data,
+          });
+        } else {
+          throw new Error("Session ID and ID not found in URL");
         }
       } catch (error) {
         console.error(error);
@@ -45,17 +78,33 @@ const App = () => {
     };
 
     handleSubmit();
-  }, []);
+  }, [location.search]);
 
   return (
     <div>
       {loading && <p>Loading...</p>}
-      {data.sessionId && (
-        <div>
-          <p>Session ID: {data.sessionId}</p>
-          <p>Response data: {JSON.stringify(data.responseData)}</p>
-        </div>
-      )}
+      {data && <p>{JSON.stringify(data)}</p>}
+
+      <p>Session ID: {data.sessionId}</p>
+      <p>ID: {data.id}</p>
+      <p>Response Data: {JSON.stringify(data.responseData)}</p>
+      <br />
+      <br />
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="passowrd"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
--- src/axios-test.jsx
+++ src/axios-test.jsx
@@ -1,90 +1,13 @@
 import axios from "axios";
 
 const firstAxiosInstance = axios.create({
-  baseURL:
-    "https://100014.pythonanywhere.com/api/userinfo/un6beoydda13mcno4kv8lsady27motnz",
+  baseURL: "https://100014.pythonanywhere.com/api/userinfo/",
 });
 
 const secondAxiosInstance = axios.create({
-  baseURL:
-    "https://100093.pythonanywhere.com/api/userinfo/un6beoydda13mcno4kv8lsady27motnz",
+  baseURL: "https://100093.pythonanywhere.com/api/userinfo/",
 });
 
 export { firstAxiosInstance, secondAxiosInstance };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { useLocation } from "react-router-dom";
-// import axios from "axios";
-
-// const firstAxiosInstance = axios.create({
-//   baseURL: "https://100014.pythonanywhere.com/api/userinfo/",
-// });
-
-// const secondAxiosInstance = axios.create({
-//   baseURL: "https://100093.pythonanywhere.com/api/userinfo/",
-// });
-
-// const App = () => {
-//   const location = useLocation();
-//   const params = new URLSearchParams(location.search);
-//   const sessionId = params.get("session_id");
-//   const id = params.get("id");
-
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       setLoading(true);
-//       let axiosInstance = firstAxiosInstance;
-//       if (id) {
-//         axiosInstance = secondAxiosInstance;
-//       }
-//       const response = await axiosInstance.post(
-//         `?session_id=${sessionId}&id=${id}`,
-//         {}
-//       );
-//       setData(response.data);
-//       setLoading(false);
-//     };
-//     if (sessionId) {
-//       fetchData();
-//     }
-//   }, [sessionId, id]);
-
-//   return (
-//     <div>
-//       {loading ? (
-//         <div>Loading...</div>
-//       ) : data ? (
-//         <div>Data: {JSON.stringify(data)}</div>
-//       ) : (
-//         <div>No data available</div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
 
